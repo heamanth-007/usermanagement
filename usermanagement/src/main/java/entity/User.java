c162d8ec621dca0b3a2fa5b82454a01676bc2e97
heamanth-007 <heamanthprabhu59@gmail.com>
2026-03-30 18:32:00 +0530

    Added JWT authentication and Lombok

--- usermanagement/src/main/java/entity/User.java
+++ usermanagement/src/main/java/entity/User.java
@@ -1,55 +1,29 @@
-package com.example.usermanagement.entity;
-import jakarta.validation.constraints.*;
 
+package com.example.usermanagement.entity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 @Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Name should not be empty")
+    @NotBlank(message="Name is required")
     private String name;
 
-    @Email(message = "Email should be valid")
-    @NotBlank(message = "Email should not be empty")
+    @Email(message="Invalid email")
     private String email;
 
-    @NotBlank(message = "Password should not be empty")
+    @NotBlank(message="Password is required")
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-// getters & setters
 }
---
+++ usermanagement/src/main/java/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+
+package com.example.usermanagement.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
